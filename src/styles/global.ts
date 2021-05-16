74eeae02ade868f77425990f6a504d5ad3e7ab01
Renato Castro <renatocastro02@hotmail.com>
2021-05-16 19:19:39 -0300

    feat: add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  outline: 0
+}
+body {
+  background: #777777;
+  color: #384052;
+  -webkit-font-smoothing: antialiased;
+}
+body, input, button {
+  font-family: 'Roboto', serif;
+  font-size: 16px;
+}
+`;
